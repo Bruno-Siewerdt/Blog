6b92cf133cc16ab49ebecc65cafa6a6591eb9902
Bruno-Siewerdt <brunosiewerdt@hotmail.com>
2025-07-25 18:16:54 -0300

    25/07/2025 - 0.0.4 - created layout for posts page; - added angular materials;

--- src/app/postFeed.ts
+++ src/app/postFeed.ts
@@ -1,11 +1,16 @@
-import { Component, signal } from '@angular/core';
+import {  HostListener, Component, signal } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'post-feed',
   templateUrl: './postFeed.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [MatButtonModule, MatCardModule]
 })
 export class PostFeed {
-    constructor(private router: Router) { }
+  constructor(private router: Router) { }
+  columnCount = signal(2);
 }
+
